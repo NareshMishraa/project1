4fd8c6aae9b9a3dd288b60f2ae8dbfea70f42c61
Naresh Mishra <naresh.mishra@bitoriotech.com>
2025-07-18 16:04:56 +0530

    Add homepage for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [
-      // base: '/your-repo-name/', // 👈 important for GitHub Pages/
-      // plugins: [react()],
-      tailwindcss(),
-      
-  ],
-})
+  base: '/project1/', // 👈 This sets the correct base path for GitHub Pages
+  plugins: [react()],
+});
