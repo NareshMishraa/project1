3635381f24430d8067eed92258007a4a12d050b4
Naresh Mishra <naresh.mishra@bitoriotech.com>
2025-07-18 16:40:15 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/project1/', // 👈 This sets the correct base path for GitHub Pages
-  plugins: [react()],
-});
+  base:'/project1/',
+  plugins: [
+      tailwindcss(),
+      react()
+      
+  ],
+})
